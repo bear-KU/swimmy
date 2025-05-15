011f70f45b38b554976393c4a78d1e56df3b59bb
bear-KU <ueda2025@s.okayama-u.ac.jp>
2025-05-15 13:52:47 +0900

    Add loc command

--- lib/swimmy/command/location.rb
+++ lib/swimmy/command/location.rb
@@ -5,77 +5,45 @@ module Swimmy
         user = client.web_client.users_info(user: data.user).user
         user_id = user.id
         user_name = user.profile.display_name
-        arg = match[:expression]
+        prefix = match[:expression]
 
-        # 所在リストを選択する
-        location_list_106 = {
-                              "hi"      => "在室",
-                              "lecture" => "講義",
-                              "meeting" => "打合",
-                              "campus"  => "学内",
-                              "outside" => "学外", 
-                              "bye"     => "帰宅"
-                            }
-        location_list_206 = {
-                              "hi"         => "在室",
-                              "meeting"    => "オンライン講義・会議中",
-                              "laboratory" => "研究室(105・106)",
-                              "lecture"    => "講義室",
-                              "departmemt" => "学科内",
-                              "campus"     => "大学内",
-                              "bye"        => "帰宅・出張"
-                            }
-        case user_name
-        when "nom"
-          location_list = location_list_206
-        else
-          location_list = location_list_106
-        end
+        user_location_info = Swimmy::Resource::UserLocation.new(user_name, prefix)
 
-        # 引数が指定されていない場合
-        if arg.nil? || arg.empty?
+        if user_location_info.prefix.nil? || user_location_info.prefix.empty?
           client.say(channel: data.channel,
                         text: "引数が指定されていません．\n" +
-                              message_location_help(location_list))
+                              user_location_info.message_location_help)
           return
         end
 
-        # 引数の先頭が一致する所在を取得する
-        prefix_match_list = retrieve_complement_location_list(arg, location_list)
+        prefix_match_list = user_location_info.retrieve_complement_location_list
 
         # 引数で指定した文字列から始まる所在の数を判定
         if prefix_match_list.length > 1
           # 引数で指定した文字列が複数の所在にマッチする場合
           client.say(channel: data.channel,
                         text: "指定された文字列に該当する所在が複数見つかりました．\n" +
-                              message_location_help(location_list)) 
-          # min_prefix_list = get_min_prefix_list(prefix_match_list.keys)
-          # prefix_match_list.each do |key, value|
-          #   client.say(channel: data.channel,
-          #                 text: "#{key}, #{value}, #{min_prefix_list[key]}")
-          # end
+                              user_location_info.message_location_help)
           return
         elsif prefix_match_list.length == 0
           # 引数で指定した文字列が所在にマッチしない場合
           client.say(channel: data.channel,
                         text: "指定された文字列に該当する所在は見つかりませんでした．\n" +
-                              message_location_help(location_list))
+                              user_location_info.message_location_help)
           return
+        else
+          # 引数で指定した文字列が1つの所在にマッチする場合
+          begin
+            doorplate_service = Swimmy::Service::Doorplate.new(mqtt_client)
+            doorplate_service.send_attendance_event(prefix_match_list.keys.first, user_id, user_name)
+          rescue Exception => e
+            client.say(channel: data.channel, text: "ドアプレートの状態を更新できませんでした．")
+            raise e
+          end
+          client.say(channel: data.channel, text: "ドアプレートの状態を #{prefix_match_list.values.first} に更新しました．")
         end
-
-        # 引数で指定した文字列が1つの所在にマッチする場合
-        begin
-          # MQTTブローカにpublish
-          doorplate_service = Swimmy::Service::Doorplate.new(mqtt_client)
-          doorplate_service.send_attendance_event(prefix_match_list.keys.first, user_id, user_name)
-        rescue Exception => e
-          client.say(channel: data.channel, text: "ドアプレートの状態を更新できませんでした．")
-          raise e
-        end
-        client.say(channel: data.channel, text: "ドアプレートの状態を #{prefix_match_list.values.first} に更新しました．")
       end
 
-      ################################################################################
       help do
         title "location"
         desc "ドアプレートの所在を変更します．"
@@ -99,58 +67,6 @@ module Swimmy
                   "campus (c) : 大学内\n" +
                   "bye (b) : 帰宅・出張\n"
       end
-
-      ################################################################################
-      # Helper Functions
-
-      # 引数に関するヘルプ文を取得
-      def self.message_location_help(location_list)
-        # プレフィックスリストを作成
-        prefix_list = get_min_prefix_list(location_list.keys)
-
-        # TODO: puts -> client.say
-        message = "引数を以下から1つを指定してください．\n\n" +
-                  "引数(最小入力) :  所在名\n"
-  
-        location_list.each do |key, value|
-          message += "#{key} (#{prefix_list[key]}) :  #{value}\n"
-        end
-        return message
-      end
-
-      # 所在リストから prefix で始まる所在を取得する
-      def self.retrieve_complement_location_list(prefix, location_list)
-        prefix_match_list = Hash::new
-        location_list.each do |key, value|
-          if key.start_with?(prefix) 
-            prefix_match_list[key] = value
-          end
-        end
-        return prefix_match_list
-      end
-
-      # 各 key を一意に識別できる最小の prefix を取得する
-      def self.get_min_prefix_list(location_keys)
-        result = Hash::new
-  
-        location_keys.each do |key|
-          min_prefix = key
-          others = location_keys.each.reject { |k| k == key }  # 自分以外にこの prefix から始まるものがあるかチェック
-          (1..key.length).each do |prefix_len|
-            prefix = key[0, prefix_len]
-            conflict = others.any? { |k| k.start_with?(prefix) }  # プレフィックスの衝突判定
-      
-            # 衝突が発生しなければリストに追加し終了
-            unless conflict
-              min_prefix = prefix
-              break
-            end
-          end
-          result[key] = min_prefix
-        end
-        return result
-      end
-
     end
   end
 end
